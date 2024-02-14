968fd70e0ee6e693ad5e4a8cedef8112394f9d38
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-02-13 21:28:49 -0800

    javascript fundamentals

---
+++ value.js
@@ -0,0 +1,69 @@
+//values and variables
+
+// var country ="america";
+// var continent ="asia";
+// var population= 4 ;
+// console.log(country,continent,population);
+
+//datatypes
+
+// let island = false;
+// let india=true;
+// let language;
+
+// console.log(typeof island);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// console.log(india);
+
+//let,const and var
+// const language="tamil";
+// language="english";
+// console.log(language);
+
+//basic operators
+// var population=100/2;
+// var country= 50/2;
+// console.log(country,population);
+
+//if/else statement
+
+// var population= 30;
+// var averagepopulation=33;
+// var average= averagepopulation-population;
+// if (population > averagepopulation){
+//     console.log("portugal's population is above average")
+// }
+// else{
+//     console.log(`portugal's population is ${average} million below average`)
+// }
+
+//type conversion and coercion
+
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 +5 + '4' +9-4-2);
+
+//switch statement
+
+var language = chinese;
+switch(language){
+    case "chinese":
+    console.log("most number of native speakers!");
+    break;
+    case "spanish":
+    console.log("2nd place in number of native speaker's");
+    break;
+    case "english":
+    console.log("3rd place");
+    break;
+    case "hindi":
+    console.log("number 4");
+    break;
+    case "arabic":
+    console.log
+}
