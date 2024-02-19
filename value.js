2c7b81f045772a8ce872732bf1d2f7640070027b
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-02-18 21:41:16 -0800

    updated tasks completly

--- value.js
+++ value.js
@@ -24,9 +24,28 @@
 // console.log(language);
 
 //basic operators
-// var population=100/2;
-// var country= 50/2;
+// var population=10;
+// var country= "portugal";
 // console.log(country,population);
+// population++;
+// console.log(population);
+// var finlandpopulation = 6;
+// if (country > finlandpopulation){
+//     console.log("country have more people than finland")
+// }
+// else{
+//     console.log("finlnd have more people than finland")
+// }
+// var averagepopulation=33;
+// if(population < averagepopulation ){
+
+//     console.log("population is less than average population")
+// }
+// else{
+//     console.log("population is greater than average population")
+// }
+// var description="portugal is in europe, and its 11million people speak portuguese";
+// console.log(description);
 
 //if/else statement
 
@@ -48,22 +67,58 @@
 // console.log('123' < 57);
 // console.log(5 +5 + '4' +9-4-2);
 
+//equality operator == vs ===
+// var numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+// if (numNeighbours == 1) {
+//     console.log("Only 1 border!");
+// } 
+// else if (numNeighbours > 1) {
+//     console.log("More than 1 border");
+// } 
+// else {
+//     console.log("No borders");
+// }
+
+var numNeighbours = prompt('how many neighbours countries does your country have?');
+console.log(typeof numNeighbours)
+if(numNeighbours === 1)
+{
+    console.log("only 1 border!");
+}
+else if(numNeighbours > 1)
+{
+    console.log("more than 1 border");
+}
+else{
+    console.log("no borders");
+}
 //switch statement
 
-var language = chinese;
-switch(language){
-    case "chinese":
-    console.log("most number of native speakers!");
-    break;
-    case "spanish":
-    console.log("2nd place in number of native speaker's");
-    break;
-    case "english":
-    console.log("3rd place");
-    break;
-    case "hindi":
-    console.log("number 4");
-    break;
-    case "arabic":
-    console.log
-}
+// var language = "chinese";
+// switch(language){
+//     case "chinese":
+//     console.log("most number of native speakers!");
+//     break;
+//     case "spanish":
+//     console.log("2nd place in number of native speaker's");
+//     break;
+//     case "english":
+//     console.log("3rd place");
+//     break;
+//     case "hindi":
+//     console.log("number 4");
+//     break;
+//     case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//     case "for all other simply":
+//     console.log("great language too");
+//     break;
+// }
+
+//The conditional ternary operator
+// var population = 40;
+// var aboveaveragemeans = population > 33 ? "portugals population is above averge" : "portugals population is below average"
+// console.log(aboveaveragemeans)
+
